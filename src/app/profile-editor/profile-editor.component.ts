16b9853276e0a1388b6767a109f8919f5b92ea48
Mauricio Francisco <mauriciodealmeidamf@gmail.com>
2020-11-23 15:21:50 -0300

    form-group-pt2

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -10,12 +10,27 @@ export class ProfileEditorComponent implements OnInit {
   profileForm = new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip: new FormControl('')
+    })
   });
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  updateProfile() {
+  this.profileForm.patchValue({
+    firstName: 'Nancy',
+    address: {
+      street: '123 Drew Street'
+    }
+  });
+}
 
   onSubmit() {
     // TODO: Use EventEmitter with form value
